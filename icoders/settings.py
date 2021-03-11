cea366c049dcb15ff8fa9451e639a225444d952e
Abdullah <iftikharabdullah281@gmail.com>
2021-03-11 17:25:52 +0500

    Initial commit

--- icoders/settings.py
+++ icoders/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '^p*0yydn70y2vv0r#cz2i^ekv(2j*e7fu1q_#jbar!(srcwl=q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "webiste-for-tech-n-game.herokuapp.com"]
 
 
 # Application definition
